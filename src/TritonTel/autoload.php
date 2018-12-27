4944c411e63846e434d9ad2f6e5db7492f90c4a6
Khatunzev-Anton <khatunzevanton@gmail.com>
2018-12-27 17:54:47 +0300

    legacy autoload path's change

--- src/TritonTel/autoload.php
+++ src/TritonTel/autoload.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once './Models/ISettings.php';
-require_once './Models/Settings.php';
-require_once './Models/SmsServiceSettings.php';
-require_once './Services/IHttpRequestService.php';
-require_once './Services/HttpRequestService.php';
-require_once './Services/SmsService/ISmsService.php';
-require_once './Services/SmsService/SmsService.php';
+require_once '/Models/ISettings.php';
+require_once '/Models/Settings.php';
+require_once '/Models/SmsServiceSettings.php';
+require_once '/Services/IHttpRequestService.php';
+require_once '/Services/HttpRequestService.php';
+require_once '/Services/SmsService/ISmsService.php';
+require_once '/Services/SmsService/SmsService.php';
 
 
 
