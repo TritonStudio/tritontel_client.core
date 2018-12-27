b5f1b58f4fc605661855b7a3976c688c384bfbde
Khatunzev-Anton <khatunzevanton@gmail.com>
2018-12-27 17:11:31 +0300

    legacy branch initial commit

--- src/TritonTel/Services/HttpRequestService.php
+++ src/TritonTel/Services/HttpRequestService.php
@@ -16,19 +16,45 @@ class HttpRequestService implements IHttpRequestService{
      * @throws ClientErrorException
      */
     public function send($httpClient, $request) {
-        try {
-            $response = $httpClient->sendRequest($request);
-        } catch (ClientErrorException $e) {
-            if ($e->getResponse()->getStatusCode() == 404) {
-                // TODO: handle errors and codes here!
+        throw new \Exception('Not impemented');
+    }
+    
+    private function sendRequest($verb, $url, $data = [], $headers = []) {
+        $content = null;
+        try{
+            $ch = curl_init();
+
+            $defaultHeaders = ["Connection: Keep-Alive", "Content-Type: application/json", "User-Agent: Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36"];
+            $headers = array_merge($defaultHeaders, $headers);
+            if($verb === 'POST'){
+                curl_setopt($ch, CURLOPT_POST,true);
+                if(empty($data)){
+                    $headers = array_merge($headers, ['Content-Length: 0']);
+                }else{
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data)); 
+                }
             }
+            curl_setopt($ch, CURLOPT_URL,$url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            //curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
+            curl_setopt($ch, CURLOPT_TIMEOUT, 1000); //timeout in seconds
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
+            
+            $content = curl_exec ($ch);
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            curl_close ($ch);
+            
+            if((!empty($httpCode)) && ($httpCode != 200)){
+                throw new \Exception('FAILED to open page:' . $url);
+            }
+        }catch(\Exception $e){
+            //add logging here
             throw $e;
         }
-        if((!$response) || ($response->getStatusCode() != 200)){
-            throw new Exception('error occurred while sending request');
-        }
-        
-        $content = $response->getBody()->getContents();
         return $content;
     }
     
@@ -38,21 +64,25 @@ class HttpRequestService implements IHttpRequestService{
      * @return HttpClient
      */
     public function getHttpClient($authentication = null){
-        return HttpClientFactory::create($authentication);
+        throw new \Exception('Not impemented');
     }
     
     public function APIRequestPOST($url, $data = null, $token = null){
-        return $this->send(
-                $this->getHttpClient((empty($token) ? (new Bearer($token)) : null)),
-                MessageFactoryDiscovery::find()->createRequest('POST', $url, [], ($data == null ? null : json_encode($data)))
-            );
+        $headers = [];
+        if(!empty($token)){
+            $headers[] = 'Authorization: Bearer ' . $token;
+        }
+        
+        return $this->sendRequest('POST', $url, $data, $headers);
     }
     
     public function APIRequestGET($url, $token = null){
-        return $this->send(
-                $this->getHttpClient((empty($token) ? (new Bearer($token)) : null)),
-                MessageFactoryDiscovery::find()->createRequest('GET', $url)
-            );
+        $headers = [];
+        if(!empty($token)){
+            $headers[] = 'Authorization: Bearer ' . $token;
+        }
+        
+        return $this->sendRequest('POST', $url, [], $headers);
     }
     
 }
